c7fa2cce766c5ec36d131c959cc3d1a7becfca6b
chaijewon <vcandjava@nate.com>
2026-09-03 15:15:49 +0900

    1111

--- src/main/java/com/sist/web/controller/FoodController.java
+++ src/main/java/com/sist/web/controller/FoodController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequiredArgsConstructor
+
 public class FoodController {
    private final FoodService fService;
    
